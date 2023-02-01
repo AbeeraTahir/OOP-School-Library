714fae65bbd98a306f582009193480816880cce8
Abeera Tahir <abeeratahir2018@gmail.com>
2023-02-02 00:48:49 +0500

    Add reviewer's suggested changes

--- person.rb
+++ person.rb
@@ -4,8 +4,8 @@ require_relative './trimmer_decorator'
 require_relative './rental'
 
 class Person < Nameable
-  attr_reader :id, :rentals
-  attr_accessor :name, :age
+  attr_reader :id
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name: 'Unknown', parent_permission: true)
     super()
