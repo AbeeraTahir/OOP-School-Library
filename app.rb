6e8a68f458f18cebe0cc71b8dfc80c8f816fa422
Abeera Tahir <abeeratahir2018@gmail.com>
2023-02-03 00:46:07 +0500

    Add implementation of methods in app.rb

--- app.rb
+++ app.rb
@@ -4,29 +4,104 @@ require_relative './book.rb'
 require_relative './rental.rb'
 
 class App
-  def initialize(user)
-    @user = user
+  def initialize(menu)
+    @menu = menu
     @books = []
     @people = []
     @rentals = []
   end
+
   def list_all_books
-    @user.show_menu
+    if @books.empty?
+      puts 'No added book!'
+    else
+      @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+    end
+    @menu.show_menu
   end
+
   def list_all_people
-    @user.show_menu
+    if @people.empty?
+      puts 'No added person!'
+    else
+      @people.each { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"}
+    end
+    @menu.show_menu
   end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp.downcase == 'y'
+    print 'Classroom: '
+    classroom = gets.chomp
+    @people.push(Student.new(classroom, age, name: name, parent_permission: parent_permission))
+    puts
+    puts 'Person created successfuly'
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    @people.push(Teacher.new(specialization, age, name: name))
+    puts
+    puts 'Person created successfuly'
+  end
+
   def create_person
-    @user.show_menu
+    puts 'Do you want to create a student(1) or a teacher(2)? [Input the number]'
+    person = gets.chomp
+    case person
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'Please add either 1 or 2!'
+    end
+
+    @menu.show_menu
   end
   def create_book
-    @user.show_menu
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    @books.push(Book.new(title, author))
+    puts 'Book created successfully'
+    @menu.show_menu
   end
+
   def create_rental
-    @user.show_menu
+    puts 'Select a book from the following list by number'
+    @books.each_with_index { |book, index| puts "#{index}) Title: '#{book.title}', Author: #{book.author}" }
+    book_number = gets.chomp.to_i
+    puts
+
+    puts 'Select a person from the following list by number (not id)'
+    @people.each_with_index { |person, index| puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+    person_number = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp
+    @rentals.push(Rental.new(date, @books[book_number], @people[person_number]))
+    puts 'Rental created successfully'
+
+    @menu.show_menu
   end
   def list_all_rentals
-    @user.show_menu
+    print 'ID of person: '
+    id = gets.chomp.to_i
+    puts 'Rentals:'
+    @rentals.each { |rental| puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}" if rental.person.id == id }
+    @menu.show_menu
   end
   def exit
     puts 'Thank you for using the app!'
--- main.rb
+++ main.rb
@@ -1,13 +1,14 @@
+#!/usr/bin/env ruby
 require_relative './app.rb'
 
-class Menu
+class MenuClass
   def initialize
     @app = App.new(self)
     puts 'Welcome to School library App!'
     show_menu
   end
   def show_menu
-    puts "\nPlease choose an option by entering a number:
+    puts "\nPlease choose an option by entering the number:
       1- List all books
       2- List all people
       3- Create a person
@@ -15,12 +16,11 @@ class Menu
       5- Create a rental
       6- List all rentals for a given person id
       7- Exit"
-      puts
       user_input = gets.chomp
       select_option(user_input)
   end
   def select_option(user_input)
-    menu = {
+    menu_list = {
       '1' => :list_all_books,
       '2' => :list_all_people,
       '3' => :create_person,
@@ -29,13 +29,18 @@ class Menu
       '6' => :list_all_rentals,
       '7' => :exit
     }
-    option = menu[user_input]
-    @app.send(option)
+    option = menu_list[user_input]
+    if option.nil?
+      puts 'Please select the number from the given list!'
+      show_menu
+    else
+      @app.send(option)
+    end
   end
 end
 
 def main
-  Menu.new
+  MenuClass.new
 end
 
 main
